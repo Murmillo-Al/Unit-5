a162a828fd04a6aa060370123cfadd258a6e4739
veloc <veloc@DESKTOP-3SAK3S5>
2022-11-16 11:44:48 -0700

    Añadiendo nueva caja de texto

--- src/main/java/com/mycompany/ejemplogithub/EjemploGitHub.java
+++ src/main/java/com/mycompany/ejemplogithub/EjemploGitHub.java
@@ -13,5 +13,6 @@ public class EjemploGitHub {
 
     public static void main(String[] args) {
         System.out.println("Bonjour! Je m'appelle Alan Murillo!");
+        System.out.println("Dodongo.");
     }
 }
